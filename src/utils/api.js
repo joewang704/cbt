5fa5abb819a14b894497ac5a831357e8ca3410b9
Joe Wang <joewang704@gmail.com>
2019-11-12 00:02:24 -0800

    ok

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import moment from 'moment'
 import axios from 'axios'
 
-const baseUrl = process.env.BACKEND_URL || 'http://localhost:4000'
+const baseUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:4000'
 
 const url = (endpoint) => `${baseUrl}/${endpoint}`
 
